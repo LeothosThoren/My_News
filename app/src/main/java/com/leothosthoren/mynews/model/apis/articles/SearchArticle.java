4d7ac79e9f7102ebb8da2ccbca2fa95287e53e57
Leothos_Thoren <soussou.med@live.fr>
2018-04-05 18:48:46 +0200

    Improve menu drawer, add links, and optimisation of web view

--- app/src/main/java/com/leothosthoren/mynews/model/apis/articles/SearchArticle.java
+++ app/src/main/java/com/leothosthoren/mynews/model/apis/articles/SearchArticle.java
@@ -1,14 +1,36 @@
 
-package com.leothosthoren.mynews.model.apis.articles;
+package com.example;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class SearchArticle {
 
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("copyright")
+    @Expose
+    private String copyright;
     @SerializedName("response")
     @Expose
     private Response response;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
 
     public Response getResponse() {
         return response;
